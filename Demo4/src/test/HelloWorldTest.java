964b4ad2488bc29dc3a8e672426f8b7818f0cce0
Vipul <click.vipul@yahoo.com>
2015-02-05 16:00:26 -0500

    Edited HelloWorldTest inside myBranch

--- Demo4/src/test/HelloWorldTest.java
+++ Demo4/src/test/HelloWorldTest.java
@@ -6,6 +6,7 @@ public class HelloWorldTest {
 		// TODO Auto-generated method stub
 		System.out.println(":::::::::::GIT is cool::::::::::");
 		System.out.println("it's easy to use");
+		System.out.println("Inside myBranch");
 
 	}
 
