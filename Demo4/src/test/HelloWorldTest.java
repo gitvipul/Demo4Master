f7cac18166d28b23006379f9b261e96e8e28545d
Vipul <click.vipul@yahoo.com>
2015-02-05 14:45:03 -0500

    Edited the HelloWorldTest File

--- Demo4/src/test/HelloWorldTest.java
+++ Demo4/src/test/HelloWorldTest.java
@@ -5,6 +5,7 @@ public class HelloWorldTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(":::::::::::GIT is cool::::::::::");
+		System.out.println("it's easy to use");
 
 	}
 
