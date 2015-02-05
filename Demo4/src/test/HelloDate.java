c8eb9938142e9298eb3ed0b639c7190c14c29ab7
Vipul <click.vipul@yahoo.com>
2015-02-05 16:06:57 -0500

    HelloDate class added to myBranch

---
+++ Demo4/src/test/HelloDate.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+package test;
+
+import java.util.*;
+
+/**
+ * @author ctsuser1
+ *
+ */
+public class HelloDate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Hello, it's: ");
+		System.out.println(new Date());
+	}
+
+}
